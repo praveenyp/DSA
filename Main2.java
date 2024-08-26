51c503dc2ab91b3d2eaed622c18ed58a2c3a40c6
praveen <praveenarya518@gmail.com>
2024-08-26 17:43:22 +0530

    first commit

---
+++ Main2.java
@@ -0,0 +1,76 @@
+// Invert the binary tree
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+class TreeNode {
+    int val;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    TreeNode(int val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+/**
+ * Main2
+ */
+public class Main2 {
+
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        // swap the left and right children
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+
+    public static void printTree(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            System.out.print(current.val + " ");
+
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.right = new TreeNode(6);
+        root.right.left = new TreeNode(7);
+
+        System.out.print("Original tree: ");
+        printTree(root);
+        System.out.println();
+
+        invertTree(root);
+        System.out.print("Inverted tree: ");
+        printTree(root);
+    }
+}
