cc7ca302f99236a3e324f2ed8059326183bd0ccd
praveen <praveenarya518@gmail.com>
2024-09-02 15:03:20 +0530

    commit

---
+++ Main7.java
@@ -0,0 +1,33 @@
+
+//LONGEST LENGTH SUB STRING...
+
+import java.util.HashMap;
+
+public class Main7 {
+    public static int longLenOfSubString(String s){
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        HashMap <Character, Integer> map =  new HashMap<>();
+
+        int maxLength = 0;
+        int left = 0;
+
+        for(int right = 0; right < s.length(); right++){
+            char currentChar = s.charAt(right);
+
+            if (map.containsKey(currentChar)) {
+                left = Math.max(map.get(currentChar) + 1, left);
+            }
+
+            map.put(currentChar, right);
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }  
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(longLenOfSubString(s));
+    }  
+}
